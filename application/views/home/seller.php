e213a51d3a39c2d5eeacf427ab1265006d574fad
EgiGrivandani <egigrivandani0133@gmail.com>
2025-03-18 10:38:18 +0700

    new update

--- application/views/home/seller.php
+++ application/views/home/seller.php
@@ -9,7 +9,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		  integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
-	<link rel="stylesheet" href="<?=base_url('assets/home/css/seller.css')?>">
+	<link rel="stylesheet" href="<?=base_url('assets/home/css/style.css')?>">
 </head>
 
 <body class="d-flex flex-column min-vh-100" style="background-image: url(<?=base_url('assets/home/image/bg-logo.png')?>);">
